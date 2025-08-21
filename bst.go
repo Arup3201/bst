bb110aa02f28e4f4eb307cd21633e9ed4df337b6
Arup3201 <arupjana7365@gmail.com>
2025-08-21 20:15:02 +0530

    feat: bst search and traversals functinality with tests

--- bst.go
+++ bst.go
@@ -36,4 +36,71 @@ func (tree *BST) Insert(z *Node) (*Node){
 	return z
 }
 
+func (tree *BST) InOrder(p []int) ([]int) {
+	return inOrderTraversal(tree.Root, p)
+}
+
+func (tree *BST) PreOrder(p []int) ([]int) {
+	return preOrderTraversal(tree.Root, p)
+}
+
+func (tree *BST) PostOrder(p []int) ([]int) {
+	return postOrderTraversal(tree.Root, p)
+}
+
+func (tree *BST) Search(t int) (node *Node, ok bool) {
+	if tree.Root == nil {
+		return (*Node)(nil), false
+	}
+
+	x := tree.Root
+	for x != nil {
+		if x.Value == t {
+			return x, true
+		} else if t > x.Value {
+			x = x.Right
+		} else {
+			x = x.Left
+		}
+	}
+
+	return (*Node)(nil), false
+}
+
+func inOrderTraversal(cn *Node, p []int) ([]int) {
+	if cn==nil {
+		return p
+	}
+
+	p = inOrderTraversal(cn.Left, p)
+	p = append(p, cn.Value)
+	p = inOrderTraversal(cn.Right, p)
+
+	return p
+}
+
+func preOrderTraversal(cn *Node, p []int) ([]int) {
+	if cn==nil {
+		return p
+	}
+
+	p = append(p, cn.Value)
+	p = preOrderTraversal(cn.Left, p)
+	p = preOrderTraversal(cn.Right, p)
+
+	return p
+}
+
+
+func postOrderTraversal(cn *Node, p []int) ([]int) {
+	if cn==nil {
+		return p
+	}
+
+	p = postOrderTraversal(cn.Left, p)
+	p = postOrderTraversal(cn.Right, p)
+	p = append(p, cn.Value)
+
+	return p
+}
 
--- bst_test.go
+++ bst_test.go
@@ -1,19 +1,9 @@
 package bst
 
 import (
-	"fmt"
+	"slices"
 	"testing"
 )
-
-func Print(st *Node) {
-	if st == nil {
-		return
-	}
-
-	Print(st.Left)
-	fmt.Printf("%d ", st.Value)
-	Print(st.Right)
-}
 
 func TestBSTInsertRoot(t *testing.T) {
 	tree, err := NewBST()
@@ -50,6 +40,84 @@ func TestBSTInsertInner(t *testing.T) {
 		}
 		tree.Insert(node)
 	}
+}
 
-	Print(tree.Root);
+func TestBSTInorder(t *testing.T) {
+	tree, err := NewBST()
+	if err != nil {
+		t.Errorf("NewBST failed to create root")
+		return
+	}
+	var nodes = []int{20, 15, 10, 9, 25, 21, 11}
+	for _, n := range nodes {
+		node, _ := NewNode(n)
+		tree.Insert(node)
+	}
+
+	got := make([]int, 0, 7)
+	got = tree.InOrder(got)
+	want := []int{9, 10, 11, 15, 20, 21, 25}
+	if !slices.Equal(got, want) {
+		t.Errorf("tree.Inorder()=%v, expected %v", got, want)
+	}
+}
+
+func TestBSTPreOrder(t *testing.T) {
+	tree, err := NewBST()
+	if err != nil {
+		t.Errorf("NewBST failed to create root")
+		return
+	}
+	var nodes = []int{20, 15, 10, 9, 25, 21, 11}
+	for _, n := range nodes {
+		node, _ := NewNode(n)
+		tree.Insert(node)
+	}
+
+	got := make([]int, 0, 7)
+	got = tree.PreOrder(got)
+	want := []int{20, 15, 10, 9, 11, 25, 21}
+	if !slices.Equal(got, want) {
+		t.Errorf("tree.Inorder()=%v, expected %v", got, want)
+	}
+}
+
+func TestBSTPostOrder(t *testing.T) {
+	tree, err := NewBST()
+	if err != nil {
+		t.Errorf("NewBST failed to create root")
+		return
+	}
+	var nodes = []int{20, 15, 10, 9, 25, 21, 11}
+	for _, n := range nodes {
+		node, _ := NewNode(n)
+		tree.Insert(node)
+	}
+
+	got := make([]int, 0, 7)
+	got = tree.PostOrder(got)
+	want := []int{9, 11, 10, 15, 21, 25, 20}
+	if !slices.Equal(got, want) {
+		t.Errorf("tree.Inorder()=%v, expected %v", got, want)
+	}
+}
+
+
+func TestBSTSearch(t *testing.T) {
+	tree, err := NewBST()
+	if err != nil {
+		t.Errorf("NewBST failed to create root")
+		return
+	}
+	var nodes = []int{20, 15, 10, 9, 25, 21, 11}
+	for _, n := range nodes {
+		node, _ := NewNode(n)
+		tree.Insert(node)
+	}
+
+	_, ok := tree.Search(10)
+	want := true
+	if ok != want {
+		t.Errorf("tree.Search()=_, %t, wanted %t", ok, want)
+	}
 }
